c26982689e2a1c4fbf6adb5f69ebb9d79445c671
charleschestnut <carcasdel1@alumn.us.es>
2018-11-27 10:29:51 +0100

    Created Whoosh practica file

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -6,6 +6,7 @@ from .models import PostProcType
 
 class PostProcView(APIView):
 
+
     def identity(self, options):
         out = []
 
@@ -24,7 +25,38 @@ class PostProcView(APIView):
 
 
     def seats(self, options, sts):
-        return self.identity(options)  # TODO
+        def calcular_cocientes(self, escanyos, votos_por_partido):
+            cocientes = []
+            for i in len(votos_por_partido):
+                cocientes.append(votos_por_partido[1] / (escanyos[i] + 1))
+
+            return cocientes
+
+        def obtener_votos(options):
+            votos = []
+            for opt in options:
+                votos.append({
+                    **opt,
+                    'postproc': opt['votes'],
+                })
+            return votos
+
+        # Cociente (S) = V/(s+1)   V: Nº total de votos de ese partido. s: Nº de escaños conseguidos, inicialmente es cero.
+        escanyos_por_partido = [0] * len(options)
+        votos_para_escaños = [0] * len(options)
+        votos_por_partido = obtener_votos(options)
+
+        for i in range(sts):
+            cocientes = calcular_cocientes(sts, votos_por_partido)
+
+            for i in range(len(votos_por_partido)):
+                votos_para_escaños[i] = cocientes[i]
+
+            partido_ganador = votos_por_partido.index(max(votos_para_escaños))
+            escanyos_por_partido[partido_ganador] += 1
+
+        return escanyos_por_partido
+
 
 
     def parity(self, options):
@@ -46,3 +78,7 @@ class PostProcView(APIView):
             return self.parity(opts)
 
         return Response({})
+
+
+
+
