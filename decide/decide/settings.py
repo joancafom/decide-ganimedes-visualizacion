0953791192843d5a48b7a8fad2a95a6fac7f6462
javirogo <javier21roga@gmail.com>
2018-12-11 11:37:13 +0100

    Update heroku config again

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -69,7 +69,7 @@ MODULES = [
 ]
 
 #BASEURL = 'http://localhost:8000'
-BASEURL = 'http://decide-javirogo.herokuapp.com'
+BASEURL = 'https://git.heroku.com/decide-javirogo.git'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
