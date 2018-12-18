c8f22060ef8f0220ee292c79a200d15a00c761fb
pablomf97 <pablomf@hotmail.es>
2018-12-18 09:59:50 +0100

    <fix>Revertido backends.py

--- decide/base/backends.py
+++ decide/base/backends.py
@@ -9,16 +9,12 @@ class AuthBackend(ModelBackend):
     '''
     This class makes the login to the authentication method for the django
     admin web interface.
-
     If the content-type is x-www-form-urlencoded, a requests is done to the
     authentication method to get the user token and this token is stored
     for future admin queries.
     '''
 
-    def authenticate(self, request, email=None, password=None, **kwargs):
-        print("HELLLOOOOOO")
-
-        email = 'root@root.com'
+    def new_authenticate(self, request, email=None, password=None, **kwargs):
         if email is None:
             email = kwargs.get(UserModel.USERNAME_FIELD)
         try:
@@ -27,17 +23,17 @@ class AuthBackend(ModelBackend):
             # Run the default password hasher once to reduce the timing
             # difference between an existing and a nonexistent user (#20760).
             UserModel().set_password(password)
-        
-        
-        u = user
+        else:
+            if user.check_password(password) and super.user_can_authenticate(user):
+                u = user
 
-        # only doing this for the admin web interface
-        if u and request.content_type == 'application/x-www-form-urlencoded':
-            data = {
-                'email': email,
-                'password': password,
-            }
-            token = mods.post('authentication', entry_point='/login/', json=data)
-            request.session['auth-token'] = token['token']
+                # only doing this for the admin web interface
+                if u and request.content_type == 'application/x-www-form-urlencoded':
+                    data = {
+                        'email': email,
+                        'password': password,
+                    }
+                    token = mods.post('authentication', entry_point='/login/', json=data)
+                    request.session['auth-token'] = token['token']
 
-        return u
+                return u
