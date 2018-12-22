65f7278ba57a4ef4ee2225eeaa8728991a950c66
Javier <javiertoronunez1997@gmail.com>
2018-12-22 01:08:15 +0100

    Actualizacion de mensajes en booth y correccion de iniciarEntorno.bat
    Se ha actualizado los nuevos mensajes introducidos ademas de incorporar
    en el script los requirements que faltan.

--- iniciarEntorno.bat
+++ iniciarEntorno.bat
@@ -1,7 +1,10 @@
 @echo off
 mode con cols=80 lines=20
 
-echo Fichero de inicializacion del entorno de decides
+echo Fichero de inicializacion del entorno de Decide Ganimedes Cabina
+
+echo Instalando requisitos
+pip install -r requirements.txt
 
 echo Moviendose a decide
 CD decide/
@@ -9,10 +12,10 @@ CD decide/
 echo Realizando makemigrations
 python manage.py makemigrations
 
-echo Migrando aplicacion
+echo Realizando migrate
 python manage.py migrate
 
-echo Creando super usuario decide@email.com
+echo Creando superusuario decide@email.com
 python manage.py createsuperuser --email decide@email.com
 
 echo Compilando mensajes de internacionalizacion (i18n)
