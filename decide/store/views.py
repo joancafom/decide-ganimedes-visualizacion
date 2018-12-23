4b4c5fafe5a9072344dac744cec8988a7681ac25
marjimveg1 <maria-2696@hotmail.com>
2018-12-23 11:32:58 +0100

    feat añadido almacenar varios votos

--- decide/store/views.py
+++ decide/store/views.py
@@ -58,15 +58,21 @@ class StoreView(generics.ListAPIView):
         if perms.status_code == 401:
             return Response({}, status=status.HTTP_401_UNAUTHORIZED)
 
-        a = vote.get("a")
-        b = vote.get("b")
+        # Creo un diccionario con todos los votos
+        defs = {}
 
-        defs = { "a": a, "b": b }
-        v, _ = Vote.objects.get_or_create(voting_id=vid, voter_id=uid,
-                                          defaults=defs)
-        v.a = a
-        v.b = b
+        # Recorro los votos
+        for v in vote:
+            # Cojo el voto
+            a = v.get("a")
 
-        v.save()
+            # Añado al diccionario el nuevo voto
+            defs[a] = 'a'
 
-        return  Response({})
+        vot, _ = Vote.objects.get_or_create(voting_id=vid, voter_id=uid,
+                                                defaults=defs)
+
+        vot.save()
+
+        return Response({})
+
