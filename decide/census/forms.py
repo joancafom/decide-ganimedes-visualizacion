2b117fcbf748f46eb4bfa1f9dad6bb5ac06dff1d
UtrillaJC <utri1990@gmail.com>
2018-12-29 20:07:48 +0100

    Mejora de censo en el formulario addCustomCensus #17
    - Antes: alguien se puede censar en una votación siempre y cuando la votación no hubiese acabado.
    - Ahora: alguien se puede censar en una votación siempre y cuando la votación no haya empezado.

--- decide/census/forms.py
+++ decide/census/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from rest_framework.exceptions import ValidationError
-
+from django.forms import ModelMultipleChoiceField
 from authentication.models import User
 from voting.models import Voting
 from django.db.models import Q
+from django.forms.widgets import MultiWidget
 
 # Contiene todos los formularios de la aplicación Census
 
@@ -15,31 +15,39 @@ NON_BINARY = 'N'
 
 DATE_INPUT_FORMATS = ['%d/%m/%Y']
 
-class CensusAddMutipleVotersForm(forms.Form):
+# Campos personalizados
+
+
+class ModelMultipleChoiceFieldByCity(ModelMultipleChoiceField):
+    def label_from_instance(self, obj):
+        return obj.__getattribute__('city')
+
+
+class CensusAddMultipleVotersForm(forms.Form):
     # Preparando valores de los atributos
 
     SEX_OPTIONS = (
         (MAN, 'Man'),
         (WOMEN, 'Woman'),
+
         (NON_BINARY, 'Non-binary'),
     )
 
-    voting = forms.ModelChoiceField(label='Seleccione una votación', empty_label="-",
-                                    queryset=Voting.objects.all().filter(start_date__isnull=False,
-                                                                         end_date__isnull=True), required=True,)
-
     # Atributos del formulario
+
+    voting = forms.ModelChoiceField(label='Seleccione una votación', empty_label="-",
+                                    queryset=Voting.objects.all().filter(start_date__isnull=True,
+                                                                         end_date__isnull=True), required=True,)
 
     sex = forms.MultipleChoiceField(label='Sex', choices=SEX_OPTIONS, required=False)
 
-    city = forms.CharField(label='City', widget=forms.TextInput(attrs={'placeholder': 'Ej: Sevilla'}), required=False)
+    # city = forms.CharField(label='City', widget=forms.TextInput(attrs={'placeholder': 'Ej: Sevilla'}), required=False)
+    # forms.TextInput (attrs={'class':'some_class', 'id':'some_id'}))
+    city = ModelMultipleChoiceFieldByCity(label='City',
+                                          queryset=User.objects.all().distinct().filter(~Q(city='')).order_by('city'),
+                                          required=False)
 
-    # TODO: Implementar city para el nombre que aparezca en la selección multiple sea city y no lo que aparece en el
-    #       método __str___
-    # city = forms.ModelMultipleChoiceField(label='City', initial='City',
-    #           queryset=User.objects.all().distinct().order_by('city'))
-
-    age_initial_range = forms.DateField(label='Edad de inicio:',
+    age_initial_range = forms.DateField(label='Edad inicial:',
                                         widget=forms.TextInput(attrs={'placeholder': 'Ej: 22/12/1990'}),
                                         input_formats=DATE_INPUT_FORMATS, required=False)
 
