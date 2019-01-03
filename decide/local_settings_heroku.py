00af98974b9f7961738162b45b00ba3a789d2ee1
javirogo <javier21roga@gmail.com>
2019-01-03 20:27:53 +0100

    Configuración local Heroku
    Añadido el archivo local de la configuración de Heroku.
    Ref #40

---
+++ decide/local_settings_heroku.py
@@ -0,0 +1,32 @@
+ALLOWED_HOSTS = ["*"]
+
+# Modules in use, commented modules that you won't use
+MODULES = [
+    'authentication',
+    'base',
+    'booth',
+    'census',
+    'mixnet',
+    'postproc',
+    'store',
+    'visualizer',
+    'voting',
+]
+
+BASEURL = 'https://ganimedes-authentication.herokuapp.com/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'decide',
+        'USER': 'decide',
+        'PASSWORD': 'decide',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+# number of bits for the key, all auths should use the same number of bits
+KEYBITS = 256
+
+APIS = {}
