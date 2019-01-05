449e64ddf30da1f0d57b6dea5e54360ee969758e
Iriabow <guilderwars@gmail.com>
2019-01-05 06:44:17 +0100

    Filtrado por edad decide-ganimedes-censo#20

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -4,12 +4,15 @@ from . import views
 
 
 urlpatterns = [
+
     path('', views.CensusCreate.as_view(), name='census_create'),
     path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail'),
     path('addAllRegistered/', views.addAllRegistered, name='addAllRegistered'),
     path('addAllInCity/', views.addAllInCity, name='addAllInCity'),
     path('addAllBySex/', views.addAllBySex, name='addAllBySex'),
+    path('addAllByAge/', views.addAllByAge, name='addAllByAge'),
     path('addCustomCensus', add_custom_census, name='addCustomCensus'),
     path('exportCensus', export_csv, name='exportCSV'),
-    path('importCensus', import_csv, name='importCSV')
+    path('importCensus', import_csv, name='importCSV'),
+
 ]
