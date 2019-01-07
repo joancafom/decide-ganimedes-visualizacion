07c095456d625904614aa9d0e58f8e0422342e0b
chainwarden16 <whiteraven1000@gmail.com>
2019-01-07 09:55:06 -0500

    Corección de errores en las plantillas #4 #21 #23
    Se ha corregido posicionamiento de labels, mejorado la vista del menú principal para distinguir entre los diferentes censos y corregido algunas erratas en las vistas.

--- decide/census/admin.py
+++ decide/census/admin.py
@@ -8,10 +8,6 @@ class CensusAdmin(admin.ModelAdmin):
     list_filter = ('voting_id', )
 
     search_fields = ('voter_id', )
-
-    delete_confirmation_template = "admin_delete_census.html"
-    object_history_template = "census_history.html"
-    change_form_template = "admin_edit_census.html"
-    change_list_template = "admin_index.html"
+    
 
 admin.site.register(Census, CensusAdmin)
