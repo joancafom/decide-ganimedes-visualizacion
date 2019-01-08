b641fb8dfb4a406bfe418c278a9cde826a5a58c5
carmen <mcarmenjara@gmail.com>
2019-01-08 18:47:33 +0100

    Se modifica el método de obtención de datos de usuario #32 para incluirla en clase APIview
    Se cambia a clase APIview y se comentan loss datos de prueba para dejarla ya operativa.

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
-from .views import Activate, obtain_auth_token, obtain_auth_token_rrss, GetUserView, LogoutView, signUp, contador, form_login
+from .views import Activate, obtain_auth_token, obtain_auth_token_rrss, GetUserView, LogoutView, signUp, GetContadorView, form_login
 
 urlpatterns = [
     path('login/', obtain_auth_token),
     path('logout/', LogoutView.as_view()),
     path('getuser/', GetUserView.as_view()),
     path('signup/', signUp),   
-    path('contador/', contador),
+    path('contador/', GetContadorView.as_view()),
     path('activate/', Activate.as_view(), name='activate'),   
     path('obtain_auth_token_rrss/', obtain_auth_token_rrss),
     path('form-login/', form_login),
