15280dadaf3d81e06f7e9743e89d2f0eec20f67c
David <davigldom@gmail.com>
2019-01-11 02:19:36 +0100

    Implementado el captcha, sin error en el test, y con los ultimos cambios
    Se ha implementado todo lo relacionado con el captcha de los commits ed4f695ec6e7dca44446825f87143ccbf6848229 y 02cbaec58e5ed72dde3614120b483d468ebfa62c en una rama derivada de develop, para incluir todos los nuevos cambios en el repositorio sin tener que lidiar de nuevo con conflictos. Ademas, se ha actualizado el test de "test_nuevo_usuario_ok" para que considere que el captcha se ha marcado, y no devuelva el "is_valid" del formulario un error ref #87

--- decide/authentication/tests.py
+++ decide/authentication/tests.py
@@ -8,6 +8,8 @@ from rest_framework.authtoken.models import Token
 from base import mods
 from .forms import *
 
+import os
+
 User=get_user_model()
 
 class AuthTestCase(APITestCase):
@@ -87,8 +89,9 @@ class AuthTestCase(APITestCase):
     
     #new user
     def test_nuevo_usuario_ok(self):
+        os.environ['NORECAPTCHA_TESTING'] = 'True'
 
-        data = {'email': 'new1@mail.com', 'firs_name': 'new', 'last_name': 'new', 'birthday':'01/01/2000', 'password1': 'practica', 'password2': 'practica', 'city': 'Sevilla'}# this user must not exits in db
+        data = {'email': 'new1@mail.com', 'first_name': 'new', 'last_name': 'new', 'birthday':'01/01/2000', 'password1': 'practica', 'password2': 'practica', 'city': 'Sevilla', 'g-recaptcha-response': 'PASSED'}# this user must not exits in db
         response = mods.get('authentication/signup', json=data, response=True) #getting the html
         self.assertEqual(response.status_code, 200)  
         response = mods.post('authentication/signup', json=data, response=True) 
